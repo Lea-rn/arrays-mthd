0720fb92c74f5c1d4a7a47a43fe2a1a081f7a67b
achrefmechergui <achreffcb123achref@gamil.com>
2025-09-30 21:12:01 +0100

    done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-///////// slice /////// 
-///// khdhit copie ; d'ont change the  original  array .... 
+///////// slice ///////
+///// khdhit copie ; d'ont change the  original  array ....
 ////// mate7ssebech el end number ..
 
 // let arr = ["a","b","c","d","e","f"]
@@ -9,8 +9,7 @@
 // console.log(arr.slice(2,4))
 // console.log(arr)
 
-
-/////// splice ////// 
+/////// splice //////
 ///// tfasakh partie ; change the original array
 
 // let arr = ["a","b","c","d","e","f"]
@@ -21,24 +20,18 @@
 
 // console.log("after" , arr)
 
-
-
-////// spread operator (...)::: 
+////// spread operator (...):::
 
 // let arr = ["a","b","c","d","e","f"]
 
-
 // const x = [
-//     {name : "flen" , country : "tunisia"} , 
-//         {name : "paul" , country : "denmark"} , 
+//     {name : "flen" , country : "tunisia"} ,
+//         {name : "paul" , country : "denmark"} ,
 // ]
-
 
 // console.log(...x)
 
-
-///// reverse //// 
-
+///// reverse ////
 
 // let arr = ["a","b","c","d","e","f"]
 
@@ -48,7 +41,7 @@
 
 // console.log(arr)
 
-////// concat ::: 
+////// concat :::
 
 // let arr = ["a","b","c","d","e","f"]
 // let x = ["j" , "k" , "l" , "m"]
@@ -59,8 +52,7 @@
 // let z = arr.concat(x.reverse())
 // console.log(z)
 
-/////// at :: 
-
+/////// at ::
 
 // let arr = ["a","b","c","d","e","f"]
 
@@ -68,48 +60,41 @@
 
 // console.log(arr.at(2))
 
+///////// for ////////////////
 
-
-///////// for //////////////// 
-
-// let numbers = [1,2,3,4,5,6,7,8,9,10] ; 
+// let numbers = [1,2,3,4,5,6,7,8,9,10] ;
 
 // for (let i = 0 ; i < numbers.length ; i++){
 //    console.log(numbers[i])
 // }
 
-
-
 // for (let i = numbers.length-1 ; i >= 0 ; i-- ){
 //     console.log(numbers[i])
 // }
 
-////// automatic for :: 
-// let numbers = [1,2,3,4,5,6,7,8,9,10] ; 
+////// automatic for ::
+// let numbers = [1,2,3,4,5,6,7,8,9,10] ;
 // for (const ele of numbers) {
 //     console.log(ele)
 // }
 
-
-// const movements = [200,450,-400,1000,-800,100] ; 
+// const movements = [200,450,-400,1000,-800,100] ;
 
 // for (const mov of movements) {
 //     if (mov > 0) {
 //         console.log(`you deposit : ${mov}`)
-//     } 
+//     }
 //     else {
-//         console.log(`you withdraw : ${Math.abs(mov)}`)  
+//         console.log(`you withdraw : ${Math.abs(mov)}`)
 //     }
 // }
 
-
-
 ////// forEach (higher order function) ///////////////
 
-const movements = [200,450,-400,1000,-800,100] ; 
+const movements = [200, 450, -400, 1000, -800, 100];
 
 ///// syntaxe :: array.forEach(function (){
-// } )   ; 
+// } )   ;
 
 // movements.forEach(function(ele , i ){
 // if (ele > 0){
@@ -119,43 +104,32 @@ const movements = [200,450,-400,1000,-800,100] ;
 // }
 // })
 
-
 ///////////// break with forEach and for /////////////////////////////////////
 // const names = ["jhon" , "peter" , "mark"] ;
 
 // for (const x of names) {
-//     if (x === "peter") break  ; 
+//     if (x === "peter") break  ;
 
 //     console.log(x)
 // }
 
 // names.forEach((name , index)=> {
-//     if (name === "peter") break ; 
+//     if (name === "peter") break ;
 //      console.log(name)
 // })
 
+///////// sort :::
 
-///////// sort ::: 
-
-// let numbers = [10,2,5,100,6,9,30] ; 
-// console.log(numbers)  
+// let numbers = [10,2,5,100,6,9,30] ;
+// console.log(numbers)
 
 // const result = numbers.sort((a,b)=> a - b )
 // console.log(result)
-
 
 // const names = ["dddd" , "cccc" , "aaaa" , "aAaa"] ;
 
 // const result = names.sort((a,b)=> a.localeCompare(b))
 // console.log(result)
-
-
-
-
-
-
-
-
 
 ///////////////////////////////////////////////////
 
@@ -165,24 +139,20 @@ const movements = [200,450,-400,1000,-800,100] ;
 //     console.log("hello")
 // }
 
-// ///// function declaration :: 
+// ///// function declaration ::
 
 // function y (){
 
 // }
 
-// ////// arrow function :: 
+// ////// arrow function ::
 
 // const z = ()=> {}
 
+// x()  ////// invocation ; call the function ; run the function
 
-// x()  ////// invocation ; call the function ; run the function 
-
-
-////// challange :: 
-////////////////////////////////////////////////// problem solving /////////////////////////////////// 
-
-
+////// challange ::
+////////////////////////////////////////////////// problem solving ///////////////////////////////////
 
 // Easy Problems::////////////////////////////////////////////
 // Append an Element:
@@ -194,21 +164,14 @@ const movements = [200,450,-400,1000,-800,100] ;
 //  arr.push(6)
 //  console.log(arr)
 
-
-
-
 ////// put your answer here ...........
 
 // Remove the Last Element:
 // Remove the last element from an array .
 
-
-
-
 ////// put your answer here ...........
 // arr.pop()
 //  console.log(arr)
-
 
 //////////////
 
@@ -223,26 +186,20 @@ const movements = [200,450,-400,1000,-800,100] ;
 // arr.unshift(0)
 // console.log(arr)
 
-
 // Remove the First Element:
 // Remove the first element from an array .
 /////////////////
-// 
+//
 ////// put your answer here ...........
 
 // arr.shift()
 // console.log(arr)
 
-
-
-
-
 // Check if Element Exists:
 // Write a function that checks if a given element exists .
 // const arr = [1,2,3,4,5]
 
-  //// boolean ==> true or false
-
+//// boolean ==> true or false
 
 ////// put your answer here ...........
 
@@ -253,15 +210,7 @@ const movements = [200,450,-400,1000,-800,100] ;
 // console.log(message)
 // }
 
-
-
 // check(20 , [5,30,20,80])
-
-
-
-
-
-
 
 // Combine two arrays into one .
 
@@ -279,10 +228,6 @@ const movements = [200,450,-400,1000,-800,100] ;
 
 // console.log(arr3)
 
-
-
-
-
 // Reverse an Array:
 // Reverse the elements in an array .
 // const arr2 = [6,7,8,9,10]
@@ -292,16 +237,6 @@ const movements = [200,450,-400,1000,-800,100] ;
 // const arr2 = [6,7,8,9,10]
 // console.log(arr2)
 // console.log(arr2.reverse())
-
-
-
-
-
-
-
-
-
-
 
 //////////////////
 
@@ -317,11 +252,6 @@ const movements = [200,450,-400,1000,-800,100] ;
 // console.log(arr2)
 // console.log(arr2.slice(2,6))
 
-
-
-
-
-
 // Splice :
 //  remove 3 elements from an array starting from index 2.
 // const arr2 = [6,7,8,9,10,20,30,50,60]
@@ -335,10 +265,6 @@ const movements = [200,450,-400,1000,-800,100] ;
 // arr2.splice(2,3)
 // console.log(arr2)
 
-
-
-
-
 // Intermediate Problems:::://///////////////////////////////////////////////
 
 // Sum of Array Elements:
@@ -346,7 +272,7 @@ const movements = [200,450,-400,1000,-800,100] ;
 
 ////// put your answer here ...........
 ///////////////////////
-// const arrOfNumbers= [1,2,3,4,5,6] ; 
+// const arrOfNumbers= [1,2,3,4,5,6] ;
 
 // const sumNumOfArray = function (arr){
 
@@ -355,36 +281,21 @@ const movements = [200,450,-400,1000,-800,100] ;
 // const sumNumOfArray = function (arr){
 //   let sum = 0 ; /// 1  // 3  // 6
 //     arr.forEach((ele)=> sum = sum + ele)   ///// 0 = 0 + 1 ; 1 = 1 + 2 ; 3 = 3 + 3
-//     return sum 
+//     return sum
 // }
 
 // console.log(sumNumOfArray(arrOfNumbers))
-  
-
-
-
-
-
-
-
-
-
 
 // Remove Element by Index:
 // Given an array, remove the element at a specific index using splice.
 ////// put your answer here ...........
 // const arrOfNumbers= [1,2,3,4,5,6]
 // console.log(arrOfNumbers)
-// let indexToRemove = 3 ; 
+// let indexToRemove = 3 ;
 
 // arrOfNumbers.splice(indexToRemove , 1 )
 
 // console.log(arrOfNumbers)
-
-
-
-
-
 
 ///////////////////////
 
@@ -393,20 +304,10 @@ const movements = [200,450,-400,1000,-800,100] ;
 ////// put your answer here ...........
 /////////////////////
 
-// const names = ["jhon" , "peter" , "mark"] 
+// const names = ["jhon" , "peter" , "mark"]
 
 // names.splice(1,2 , 10 , 50)
 // console.log(names)
-
-
-
-
-
-
-
-
-
-
 
 // Extract Last 3 Elements:
 // Use slice to get the last 3 elements from an array.
@@ -417,18 +318,11 @@ const movements = [200,450,-400,1000,-800,100] ;
 
 ////////////////////
 
-
-
-
-
-
-
-
 // Concatenate and Sort:
 // Concatenate two arrays, then sort the result in ascending order.
 ////// put your answer here ...........
 ////////////////////
-// const arr1 = [5,3,2,1,4] 
+// const arr1 = [5,3,2,1,4]
 // const arr2 = [9,8,6,10,7]
 // console.log(arr1)
 // console.log(arr2)
@@ -437,81 +331,76 @@ const movements = [200,450,-400,1000,-800,100] ;
 
 // console.log(arr3)
 
-
-
-
-
-
-
-
-
-
-
-
-
 // Find Maximum Number:
 // Use forEach to find the largest number in an array.
 ////// put your answer here ...........
 
 ////////////////
-const arr2 = [0,8,6,10,7 ,11 ,13]
+const arr2 = [0, 8, 6, 10, 7, 11, 13];
 
-///// first method :: 
+///// first method ::
 
-function max (arr){
-let maxNumber = arr[0] ;    //// 13
-  arr.forEach((num)=> {
-    if (num > maxNumber) {
-        maxNumber = num
-    }
-  })
-  return maxNumber
-}
+// function max (arr){
+// let maxNumber = arr[0] ;
+//   arr.forEach((num)=> {
+//     if (num > maxNumber) {
+//         maxNumber = num
+//     }
+//   })
+//   return maxNumber
+// }
 
-console.log(arr2)
-console.log(max(arr2))
+// console.log(arr2)
+// console.log(max(arr2))
 
+///// second method ::
+// console.log(arr2)
 
-
-
-
-
-
-
-
-
-
-
-
-
+// arr2.sort((a,b)=> b-a)
+// console.log(arr2[0])
 
 // Count Occurrences of a Value:
 // Write a function that counts how many times a specific value appears in an array using forEach.
-
 
 ////// put your answer here ...........
 
 // const numbers = [1,10,2,10,5,6,10,100,6]
 
+// function countElement (arr , value){
+//  let count = 0 ;
+//  arr.forEach((ele)=> {   /// 10
+// if (ele === value) {
+//  count ++  /// count = count +1
+// }
+//  })
 
+// return count
 
+// }
 
-
-
+// console.log(countElement(numbers , 10))
 
 // Filter Negative Numbers:
 // Use forEach  to remove all negative numbers from an array.
 
 ////// put your answer here ...........
 // const numbers = [-1,-2,-3,10,-100,6,9,70]  ;
-
+// console.log(numbers)
 
 ////////////////////
 
+// function posituveNumber (arr){
+// let positive = [] ;
+// arr.forEach((num)=> {
+//   if (num > 0 ){
+//     positive.push(num)
+//   }
+// })
+// return positive
 
+// }
 
-
-
+// console.log(posituveNumber(numbers))
 
 // Flatten an Array of Arrays:
 // Use concat and forEach to flatten an array of arrays (e.g., [[1, 2], [3, 4]] into [1, 2, 3, 4]).
@@ -521,9 +410,16 @@ console.log(max(arr2))
 
 ////// put your answer here ...........
 
-
-
-
+// const arr = [[1,2,3],[10,20,30],[50,60,100]]
+// console.log(arr)
+// function flatten (arr){
+//   let flat = [] ; 
+//   arr.forEach((ele)=> {
+//     flat = flat.concat(ele)
+//   })
+//   return flat
+// }
+// console.log(flatten(arr))
 
 // Advanced Problems:://////////////////////////////////////////////////////////////////////////////
 
@@ -531,29 +427,58 @@ console.log(max(arr2))
 // Use forEach and includes to remove duplicates from an array.
 ////// put your answer here ...........
 // const numbers = [2,10,5,7,10,8,50,10,50,8,200] ;
+// console.log(numbers)
+
+// function removeDuplicates (array){
+// let withoutDouble = [] ;  // [2,10,5,7] 
+// array.forEach((ele)=> {  // 2 
+//   if (!withoutDouble.includes(ele)){
+//     withoutDouble.push(ele)
+//   }
+// })
+// return withoutDouble
+// }
 
 
-
-
-
-
+// console.log(removeDuplicates(numbers))
 //////////////////////////////////
 
 // Create a New Array Without Specific Elements:
 // Write a function that removes all occurrences of a specific value from an array using forEach and splice.
 ////// put your answer here ...........
 
+// const numbers = [2,10,5,7,10,8,50,10,50,8,200] ;
+
+// function removeElement (arr , value){
+//  arr.forEach((ele,i)=> {  
+//   if (ele === value) {
+//     arr.splice(i,1)
+//   }
+//  })
+//  return arr
+// }
 
 
-
+// console.log(numbers)
+// console.log(removeElement(numbers,10))
 
 
 // Sort an Array in Descending Order:
 // Create a function that sorts an array in descending order using reverse and the sort method.
 ////// put your answer here ...........
 
-
-
-
 //////////////////////////////////////
 
+// const numbers = [-1,-2,-3,10,-100,6,9,50]
+
+// function descending (arr){
+//// first methode : 
+// const result = arr.sort((a,b)=> b - a)
+
+//// second method : 
+// const result = arr.sort((a,b)=> a-b).reverse()
+// return result
+// }
+
+// console.log(numbers)
+// console.log(descending(numbers))
